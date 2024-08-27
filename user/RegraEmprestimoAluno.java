c106d34468ab5734b892b7b7061cba70179020fa
fabriciosilvas <fabriciosilva@ufba.br>
2024-08-27 16:42:06 -0300

    Criação da classe RegraEmprestimoAluno

---
+++ user/RegraEmprestimoAluno.java
@@ -0,0 +1,56 @@
+package user;
+import book.*;
+
+public class RegraEmprestimoAluno extends RegraEmprestimo {
+	private static RegraEmprestimoAluno minhaInstancia;
+	
+	private RegraEmprestimoAluno() {
+		
+	}
+	
+	public static RegraEmprestimo getInstancia() {
+		if (minhaInstancia == null)
+			minhaInstancia = new RegraEmprestimoAluno();
+		
+		return minhaInstancia;
+	} 
+
+	@Override
+	public boolean haExemplarDisponivel(Usuario usuario, Livro livro) {
+		
+		if (livro.getQtdExemplaresDisponiveis() > 0) {
+			
+			if (livro.getQtdReservasFeitas() >= livro.getQtdExemplaresDisponiveis()) {
+				if (livro.haReservaFeita(usuario)) {
+					
+					return true;
+				}
+				
+				System.out.println("Motivo: há exemplares disponíveis apenas para quem fez reserva.");
+				
+				return false;
+			}
+			
+			return true;
+		}
+		
+		System.out.println("Motivo: não há exemplares disponíveis para empréstimo");
+		
+		return false;
+	}
+
+	@Override
+	public boolean verificarCondicoesAdicionais(Usuario usuario, Livro livro) {
+		return verificarLimiteDeLivros(usuario);
+	}
+	
+	public boolean verificarLimiteDeLivros(Usuario usuario) {
+		Aluno aluno = (Aluno)usuario;
+		if (usuario.getQtdLivrosEmprestados() == aluno.getMaxLivrosEmprestimo()) {
+			System.out.printf("Motivo: limite de empréstimo atingido");
+			return false;
+		}
+				
+		return true;
+	}
+}
