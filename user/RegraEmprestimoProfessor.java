91819f6a1ca927e66cea76b7a8208b8cdfabd43c
fabriciosilvas <fabriciosilva@ufba.br>
2024-08-27 16:42:41 -0300

    Criação da classe RegraEmprestimoProfessor

---
+++ user/RegraEmprestimoProfessor.java
@@ -0,0 +1,33 @@
+package user;
+
+import book.Livro;
+
+public class RegraEmprestimoProfessor extends RegraEmprestimo {
+	private static RegraEmprestimoProfessor minhaInstancia;
+	
+	private RegraEmprestimoProfessor() {
+		
+	}
+	
+	public static RegraEmprestimo getInstancia() {
+		if (minhaInstancia == null)
+			minhaInstancia = new RegraEmprestimoProfessor();
+		
+		return minhaInstancia;
+	} 
+	
+	@Override
+	public boolean haExemplarDisponivel(Usuario usuario, Livro livro) {
+		if (livro.getQtdExemplaresDisponiveis() > 0) 
+			return true;
+		
+		System.out.println("Motivo: não há exemplares disponíveis para empréstimo");
+		
+		return false;
+	}
+
+	@Override
+	public boolean verificarCondicoesAdicionais(Usuario usuario, Livro livro) {
+		return true;
+	}	
+}
