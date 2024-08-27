6d611bed90be0e003f835e83efa3cd3cf2d87d2e
fabriciosilvas <fabriciosilva@ufba.br>
2024-08-27 16:41:26 -0300

    Criação da classe RegraEmprestimo

---
+++ user/RegraEmprestimo.java
@@ -0,0 +1,24 @@
+package user;
+import java.util.Calendar;
+import java.util.List;
+import book.*;
+
+public abstract class RegraEmprestimo {
+	
+	public abstract boolean haExemplarDisponivel(Usuario usuario, Livro livro);
+	
+	public abstract boolean verificarCondicoesAdicionais(Usuario usuario, Livro livro);
+	
+	public boolean verificarCondicoesEmprestimo(Usuario usuario, Livro livro) {
+		return (!this.usuarioEmDebito(usuario)) && (!this.haEmprestimoEmCurso(usuario, livro)) &&
+				this.haExemplarDisponivel(usuario, livro) && this.verificarCondicoesAdicionais(usuario, livro);
+	}
+	
+	public boolean usuarioEmDebito(Usuario usuario) {
+		return usuario.usuarioEmDebito();
+	}
+	
+	public boolean haEmprestimoEmCurso(Usuario usuario, Livro livro) {
+		return livro.haEmprestimoEmCurso(usuario);
+	}
+}
