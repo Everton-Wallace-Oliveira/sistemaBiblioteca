a861fb8aebdb4db473baa88dec6b8ee380e02fb5
fabriciosilvas <fabriciosilva@ufba.br>
2024-08-30 11:25:56 -0300

    Finalização da classe SistemaBiblioteca

--- program/SistemaBiblioteca.java
+++ program/SistemaBiblioteca.java
@@ -1,23 +1,37 @@
 package program;
 import java.util.HashMap;
 
-import command.Comando;
-import command.ComandoConsultarLivro;
-import command.ComandoDevolucao;
-import command.ComandoEmprestimo;
-import command.Parametros;
+import command.*;
 
-public class Sistema {
-	private HashMap<String, Comando> comandos = new HashMap<String, Comando>();
-
-	private void initComandos() {
-		comandos.put("emp", new ComandoEmprestimo());
-		comandos.put("dev", new ComandoDevolucao());
-		comandos.put("liv", new ComandoConsultarLivro());
+public class SistemaBiblioteca {
+	private HashMap<String, Comando> comandos;
+	private static SistemaBiblioteca minhaInstancia;
+	
+	private SistemaBiblioteca() {
+		this.comandos = new HashMap<String, Comando>();
+		this.inicializaComandos();
+	}
+	
+	public static SistemaBiblioteca getInstancia() {
+		if (minhaInstancia == null)
+			minhaInstancia = new SistemaBiblioteca();
+		
+		return minhaInstancia;
+	}
+	
+	private void inicializaComandos() {
+		this.comandos.put("emp", new ComandoEmprestimo());
+		this.comandos.put("dev", new ComandoDevolucao());
+		this.comandos.put("liv", new ComandoConsultarLivro());
+		this.comandos.put("res", new ComandoReserva());
+		this.comandos.put("usu", new ComandoConsultarUsuario());
+		this.comandos.put("ntf", new ComandoConsultaNotificacao());
+		this.comandos.put("obs", new ComandoObservaReserva());
+		this.comandos.put("sai", new ComandoEncerramento());
 	}
 	
 	public void executarComando(String strComando, Parametros parametros) {
-		Comando comando = comandos.get(strComando);
+		Comando comando = this.comandos.get(strComando);
 		comando.executar(parametros);
 	}
 }
