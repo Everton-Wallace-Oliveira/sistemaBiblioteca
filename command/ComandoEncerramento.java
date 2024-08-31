6b1340958171d2b114e81ff0ad8196957dbc9e93
EvertonWallace <everton542@hotmail.com>
2024-08-31 20:56:57 -0300

    criacao da classe encerramento

--- command/ComandoEncerramento.java
+++ command/ComandoEncerramento.java
@@ -1,5 +1,11 @@
 package command;
 
-public class ComandoEncerramento {
+public class ComandoEncerramento implements Comando {
 
+	@Override
+	public void executar(Parametros parametros) {
+		System.out.println("Programa finalizado!");
+		System.exit(0);
+	}
+	
 }
